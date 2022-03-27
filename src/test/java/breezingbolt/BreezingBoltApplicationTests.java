dbe4429c35a6d727caa9cc3039cf91ef035d5300
Akalanka Perera <akalanka@parallax.lk>
2022-03-27 14:35:04 +0530

    Added staging workflow

--- src/test/java/breezingbolt/BreezingBoltApplicationTests.java
+++ src/test/java/breezingbolt/BreezingBoltApplicationTests.java
@@ -1,13 +1,23 @@
 package breezingbolt;
 
+import breezingbolt.http.controllers.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BreezingBoltApplicationTests {
 
+    @Autowired
+    HomeController homeController;
+
     @Test
     void contextLoads() {
+        ModelAndView home = homeController.getHomePage();
+        assertEquals(HttpStatus.OK, home.getStatus());
     }
 
 }
